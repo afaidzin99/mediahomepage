f594e4e1f2d8e58157c3d96efbd6682b4e71197c
vzrenggamani <rengga.prakoso@gmail.com>
2024-02-26 15:11:41 +0700

    feat: make header animated while scrolling

--- app/header.tsx
+++ app/header.tsx
@@ -1,9 +1,31 @@
 "use client";
 import Link from "next/link";
+import { useState, useEffect } from "react";
 
 export default function Header() {
+  const [navbarColor, setNavbarColor] = useState("bg-green-primary");
+
+  useEffect(() => {
+    const changeNavbarColor = () => {
+      if (window.scrollY >= 80) {
+        setNavbarColor("bg-greenBrand");
+      } else {
+        setNavbarColor(
+          "bg-green-primary sm:h-16 lg:h-20 lg:bg-black lg:bg-opacity-10 lg:backdrop-filter lg:backdrop-blur-lg lg:hover:bg-green-primary"
+        );
+      }
+    };
+
+    window.addEventListener("scroll", changeNavbarColor);
+
+    return () => {
+      window.removeEventListener("scroll", changeNavbarColor);
+    };
+  }, []);
   return (
-    <header className="flex items-center w-screen h-12 fixed top-0 z-50 bg-green-primary sm:h-16 lg:h-20 transition-colors ease-brand duration-250 lg:bg-black lg:bg-opacity-10 lg:backdrop-filter lg:backdrop-blur-lg lg:hover:bg-green-primary bg-greenBrand">
+    <header
+      className={`flex items-center w-screen h-12 fixed top-0 z-50 ease-in-out transition-all duration-200 ${navbarColor}`}
+    >
       <div className="container mx-auto px-6 2xl:px-0 xl:max-w-7xl">
         <nav className="flex items-center">
           <Link href={"/"} className="font-bold text-white">
