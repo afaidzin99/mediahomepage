49bafa1755d8c3a547ce699cdbd018ff9e1d76fa
vzrenggamani <rengga.prakoso@gmail.com>
2024-04-02 05:05:45 +0700

    feat: add quick access link to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HomepageHero from "./homepageHero";
 import HomepageLatestNews from "./homepageLatestNews";
+import HomepageQuickAcces from "./homepageQuickAcces";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <HomepageHero />
       <HomepageLatestNews />
+      <HomepageQuickAcces />
     </main>
   );
 }
