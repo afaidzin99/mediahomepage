0e3c9eefa9b1edc7cc0104337ba40bddc5b21825
vzrenggamani <rengga.prakoso@gmail.com>
2024-04-03 12:56:10 +0700

    chore: add metadata update for main layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
 import HomepageHero from "./homepageHero";
 import HomepageLatestNews from "./homepageLatestNews";
 import HomepageQuickAcces from "./homepageQuickAcces";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Media Sawocangkring - Media Informasi Seputar Desa Sawocangkring",
+  description:
+    "Media Informasi Seputar Desa Sawocangkring. Dikelola oleh tim Pengembang Media Sawocangkring.",
+};
 
 export default function Home() {
   return (
